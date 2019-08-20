399c308bb030f55e522a0631557d5dd4c0a503f0
Aneta76 <aneta_d88@o2.pl>
2019-08-20 20:38:24 +0200

    simple tasks based on List and String classes

--- src/main/java/Company.java
+++ src/main/java/Company.java
@@ -1,4 +1,5 @@
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 
 public class Company {
@@ -9,6 +10,7 @@ public class Company {
         employees.add("Marcin Marciński, 34, 4000");
         employees.add("Zenek Zenkowski, 55, 6450");
         employees.add("Kot Kotowski, 44, 3533");
+        employees.add("Pies Piesowski, 44, 3533");
         employees.add("Maciek Maciecki, 28, 9000");
         employees.add("Jan Jankowski, 29, 4000");
         employees.add("Włodek Włodkowski, 76, 5000");
@@ -60,12 +62,102 @@ public class Company {
         }
     }
 
+    public void dismissAnimals(List<String> employees) {
+        String[] animals = {"pies", "kot", "mysz", "slon", "ptak", "lew"};
+        for (int i = 0; i < employees.size(); i++) {
+            String employee = employees.get(i).toLowerCase();
+            for (int j = 0; j < animals.length; j++) {
+                if (employee.contains(animals[j])) {
+                    employees.remove(employees.get(i));
+                    i--;
+                    System.out.println("Animal " + animals[j] + " has been dismissed from the company!");
+                }
+            }
+        }
+    }
+
+    public void raiseSalaryForMen(List<String> employees) {
+        String[] singleEmp;
+        String[] empData;
+        double salary;
+        for (int i = 0; i < employees.size(); i++) {
+            String employee = employees.get(i);
+            singleEmp = employee.split(",");
+            salary = Double.parseDouble(singleEmp[2].trim());
+            String fullName = singleEmp[0].trim().toLowerCase();
+            empData = fullName.split(" ");
+            if (!empData[0].endsWith("a")) {
+                salary = salary + 100;
+                System.out.println("New salary of " + fullName + " is: " + salary);
+            }
+        }
+    }
+
+
+    public void delegateEmployee(List<String> employees) {
+        String[] singleEmp;
+        String[] empData;
+        double salary;
+        for (int i = 0; i < employees.size(); i++) {
+            String employee = employees.get(i);
+            singleEmp = employee.split(",");
+            salary = Double.parseDouble(singleEmp[2].trim());
+            String fullName = singleEmp[0].trim().toLowerCase();
+            empData = fullName.split(" ");
+            String lastName = empData[1].trim();
+            if (lastName.length() > 7) {
+                salary = salary / 4.3;
+                System.out.println("Salary of " + fullName + " is: " + salary + " euro");
+            }
+        }
+    }
+
+    public void countAnnualSalary(List<String> employees) {
+        double salary;
+        double annualSalary;
+        double extraSalary;
+        String[] singleEmp;
+        for (int i = 0; i < employees.size(); i++) {
+            String employee = employees.get(i);
+            singleEmp = employee.split(",");
+            salary = Double.parseDouble(singleEmp[2].trim());
+            annualSalary = salary * 12;
+            if (annualSalary < 70000) {
+                extraSalary = annualSalary * 0.1;
+                System.out.println("Extra salary: " + extraSalary + " " + Arrays.toString(singleEmp));
+            }
+        }
+    }
+
+    public void goHome(List<String> employees) {
+        String[] singleEmp;
+        double retirement = 0;
+        double salary;
+        for (int i = 0; i < employees.size(); i++) {
+            String employee = employees.get(i);
+            singleEmp = employee.split(",");
+            salary = Double.parseDouble(singleEmp[2].trim());
+            int age = Integer.parseInt(singleEmp[1].trim());
+            if (age >= 55) {
+                retirement = (salary * 1.3) * 0.5;
+                employees.remove(employee);
+                i--;
+                System.out.println("retirement: " + retirement);
+            }
+        }
+    }
+
     public static void main(String[] args) {
         Company company = new Company();
-        List <String> employees =  company.createEmployeesList();
+        List<String> employees = company.createEmployeesList();
         int averageAge = company.countAverageAge(employees);
         double averageSalary = company.countAverageSalary(employees);
-        company.raiseSalaryForWomen(averageSalary,averageAge,employees);
+        //company.raiseSalaryForWomen(averageSalary,averageAge,employees);
+        //company.dismissAnimals(employees);
+        // company.raiseSalaryForMen(employees);
+        //company.delegateEmployee(employees);
+        //company.countAnnualSalary(employees);
+        company.goHome(employees);
     }
 }
 //    I. Zadeklaruj listę pracowników firmy (lista stringów):
@@ -89,6 +181,6 @@ public class Company {
 //        c) podniesie wynagrodzenie kobietom o 20%, które są młodsze niż średnia wieku oraz mają niższe wynagrodzenie od średniego
 //        d) zwolni zwierzęta z firmy (usunie je z listy)
 //        e) Wszystkim mężczyznom podniesie wynagrodzenie o 100zł
-//        f) Wyśle pracowników którzy mają nazwisko dłuższe niż 7 znaków do pracy za granicą. Tacy pracownicy dostają wynagrodzenie w euro. Przelicz ich wynagrodzenie, przyjmując 1zł = 4.30 Euro
-//        g) policzy roczne wynagrodzenie każdego pracownika. Jeżeli będzie niższe niż 7000, pracownik dostaje "trzynastkę" równą 10% jego wynagrodzenia netto.
+//        f) Wyśle pracowników którzy mają nazwisko dłuższe niż 7 znaków do pracy za granicą. Tacy pracownicy dostają wynagrodzenie w euro. Przelicz ich wynagrodzenie, przyjmując 1euro = 4.30 zl
+//        g) policzy roczne wynagrodzenie każdego pracownika. Jeżeli będzie niższe niż 70000, pracownik dostaje "trzynastkę" równą 10% jego wynagrodzenia netto.
 //        h) Wyśle pracownika na emeryturę. Pracownik może iść na emeryturę wtedy, gdy ma przynajmniej 55 lat. Dostaje wtedy emeryturę równą 50% swojego wynagrodzenia brutto oraz jest usuwany z listy.
