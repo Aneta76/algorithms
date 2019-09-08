f7a59efbe3292765624ab27ff6a9c9bd28c2f11d
Aneta76 <aneta_d88@o2.pl>
2019-09-08 15:39:14 +0200

    find dividers and primary numbers

--- src/main/java/PrimaryNumbers.java
+++ src/main/java/PrimaryNumbers.java
@@ -5,25 +5,42 @@ public class PrimaryNumbers {
 
         for (int i = 1; i <= 100; i++) { //liczba, ktora sprawdzamy
             isPrimary = true;
-            for (int a = 1; a <= i ; a++) { //liczba, przez ktora dzielimy
-                if(i % a == 0){
+            for (int a = 1; a <= i; a++) { //liczba, przez ktora dzielimy
+                if (i % a == 0) {
                     isPrimary = false;
                 }
-
             }
-            if(isPrimary) {
+            if (isPrimary) {
                 System.out.println(i);
             }
-
-
         }
+    }
 
+    public void findDividers() {
+        for (int i = 1; i <= 30; i++) {
+            int count = 0;
+            System.out.print("Number " + i + " ,");
+            for (int j = 1; j <= i; j++) {
+                if (i % j == 0) {
+                    System.out.print(j + " ");
+                    count++;
+                }
+            }
+            if (count <= 2) {
+                System.out.println("Primary Number");
+            }
+            System.out.println();
+        }
     }
 
     public static void main(String[] args) {
         PrimaryNumbers primaryNumbers = new PrimaryNumbers();
         primaryNumbers.printPrimaryNumbers();
+        primaryNumbers.findDividers();
     }
 }
+
+// Dla każdej liczby naturalnej z zakresu 1-30 wypisz jej wszystkie całkowite dzielniki.
+// Jeżeli liczba jest pierwsza, wyświetl stosowny komunikat.
 
 // wypisz wszystkie liczby pierwsze z zakresu 1-100.
