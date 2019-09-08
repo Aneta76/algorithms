5933d7a1e0a914d144d44de4a9100b681b7aaf4b
Aneta76 <aneta_d88@o2.pl>
2019-09-08 15:46:39 +0200

    fixed wrong algorithm

--- src/main/java/PrimaryNumbers.java
+++ src/main/java/PrimaryNumbers.java
@@ -5,7 +5,7 @@ public class PrimaryNumbers {
 
         for (int i = 1; i <= 100; i++) { //liczba, ktora sprawdzamy
             isPrimary = true;
-            for (int a = 1; a <= i; a++) { //liczba, przez ktora dzielimy
+            for (int a = 2; a < i; a++) { //liczba, przez ktora dzielimy
                 if (i % a == 0) {
                     isPrimary = false;
                 }
@@ -36,7 +36,7 @@ public class PrimaryNumbers {
     public static void main(String[] args) {
         PrimaryNumbers primaryNumbers = new PrimaryNumbers();
         primaryNumbers.printPrimaryNumbers();
-        primaryNumbers.findDividers();
+        //primaryNumbers.findDividers();
     }
 }
 
