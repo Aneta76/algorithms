4beb2918342ed5ae613c8c2845c781f0f465880b
Aneta76 <aneta_d88@o2.pl>
2019-09-12 21:05:34 +0200

    Fibonacci algorithm

--- src/main/java/Fibonacci.java
+++ src/main/java/Fibonacci.java
@@ -1,6 +1,6 @@
 public class Fibonacci {
 
-    public int countFibonacci(int n) {
+    public int countFibonacciRecursive(int n) {
         int result;
         if (n == 0) {
             return 0;
@@ -8,12 +8,32 @@ public class Fibonacci {
         if (n == 1) {
             return 1;
         }
-        result = countFibonacci(n - 1) + countFibonacci(n - 2);
+        result = countFibonacciRecursive(n - 1) + countFibonacciRecursive(n - 2);
+        return result;
+    }
+
+    public int countFibonacci(int n) {
+        int fiboZero = 0;
+        int fiboOne = 1;
+        int result = 0;
+        if (n > 1) {
+            for (int i = 1; i < n; i++) {
+                result = fiboOne + fiboZero;
+                fiboZero = fiboOne;
+                fiboOne = result;
+
+            }
+        } else if (n == 1) {
+            return 1;
+        } else {
+            return 0;
+        }
         return result;
     }
 
     public static void main(String[] args) {
         Fibonacci fibonacci = new Fibonacci();
-        System.out.println(fibonacci.countFibonacci(16));
+        //  System.out.println(fibonacci.countFibonacciRecursive(16));
+        System.out.println(fibonacci.countFibonacci(7));
     }
 }
