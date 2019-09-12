4e43785560645f324f8de45770b72e4618e590e3
Aneta76 <aneta_d88@o2.pl>
2019-09-12 19:52:27 +0200

    count strong recursive

--- src/main/java/Strong.java
+++ src/main/java/Strong.java
@@ -9,10 +9,19 @@ public class Strong {
         System.out.println(result);
     }
 
+    public int countStrongRecursive(int n) {
+        if (n == 1) {
+            return 1;
+        }
+        return n * countStrongRecursive((n - 1));
+    }
+
     public static void main(String[] args) {
         Strong strong = new Strong();
         strong.countStrong(6);
+        System.out.println(strong.countStrongRecursive(6));
     }
 }
 
 //  Napisz algorytm obliczający silnię z 6 (nierekurencyjnie)
+//  Napisz algorytm obliczający silnię z 6 (rekurencyjnie)
